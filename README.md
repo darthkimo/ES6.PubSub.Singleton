dd2e9c32419796711cbff50bc855a6d310c6ec5f
Wei-Hsin Chen <weihsinc@google.com>
2020-12-27 17:31:40 -0800

    Creating PubSub Singleton Class

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# ES6.PubSub.Singleton
+# PubSub Singleton Class
+
+A lightweight es6 PubSub class (Singleton).
+
+## Usage
+### Import the PubSub singleton
+
+```javascript
+import PubSub from './PubSub.js';
+
+const subscription = PubSub.subscribe( 'event1', testSubscriber );
+```
+
+### Subscribe to new event with a custom callback
+```javascript
+PubSub.subscribe('event', callback)
+```
+
+### Publish/broadcast an event and pass optional arguments
+```javascript
+PubSub.publish('event', {data})
+```
+
+### Unsubscribe from a specific topic
+```javascript
+
+PubSub.unsubscribe(subscription);
+```
---
+++ pubsub.js
@@ -0,0 +1,96 @@
+/**
+ * PubSub (publish/subscribe) Singleton Class
+ */
+class PubSub {
+  /**
+   * PubSub initialization.
+   */
+  constructor() {
+    /**
+     * Storage for topics_ that can be broadcast or listened to
+     * @private
+     * @type {Object}
+     */
+    this.topics_ = {};
+    /**
+     * A topic identifier
+     * @private
+     * @type {number}
+     */
+    this.subUid_ = -1;
+  }
+
+  /**
+   *  Subscribe to events of interest with a specific topic name and
+   *  a callback function, to be executed when the topic/event is observed
+   * @public
+   * @param {string} topic
+   * @param {function} callback
+   * @return {string} token
+   */
+  subscribe(topic, callback) {
+    if (!this.topics_[topic]) {
+      this.topics_[topic] = [];
+    }
+    // eslint-disable-next-line no-plusplus
+    const token = (++this.subUid_).toString();
+    this.topics_[topic].push({
+      token: token,
+      callback: callback,
+    });
+    return token;
+  }
+
+  /**
+   * Publish or broadcast events of interest with a specific topic name
+   * and arguments such as the data to pass along
+   * @public
+   * @param {string} topic
+   * @param {*} [args]
+   * @return {PubSub|boolean}
+   */
+  publish(topic, args) {
+    if (!this.topics_[topic]) {
+      return false;
+    }
+    const subscribers = this.topics_[topic];
+    let len = subscribers ? subscribers.length : 0;
+    // eslint-disable-next-line no-plusplus
+    while (len--) {
+      subscribers[len].callback(topic, args);
+    }
+    return this;
+  }
+
+  /**
+   * Unsubscribe from a specific topic, based on a tokenized reference
+   * to the subscription
+   * @public
+   * @param {string} token
+   * @return {PubSub|string}
+   */
+  unsubscribe(token) {
+    for (const topic in this.topics_) {
+      if (this.topics_[topic]) {
+        this.topics_[topic].forEach((topic, i) => {
+          if (topic.token === token) {
+            this.topics_[topic].splice(i, 1);
+            return token;
+          }
+        });
+      }
+    }
+    return this;
+  }
+
+  /**
+   * Get current subscribed topics list
+   * @public
+   * @return {Object}
+   */
+  getTopics() {
+    return this.topics_;
+  }
+}
+
+export default new PubSub;
